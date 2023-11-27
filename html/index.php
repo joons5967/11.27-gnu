489154cf58d6761b61a4a7228068f792452ef13e
joons5967 <dlwnstjd5967@nate.com>
2023-11-27 17:42:55 +0900

    저장이여

---
+++ html/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include_once("./navi.php"); ?>
+    <h3>첫페이지입니다.</h3>
+</body>
+</html>
---
+++ html/navi.php
@@ -0,0 +1,5 @@
+<ul>
+    <li><a href="./about.php">회사소개</a></li>
+    <li><a href="./contact.php">연락처</a></li>
+    <li><a href="./index.php">홈</a></li>
+</ul>
